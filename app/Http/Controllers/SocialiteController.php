7d3890de832bb28c6d9f486fd73c0ab97ab19725
Erivelton Silva <eriveltondasilva13@gmail.com>
2023-12-11 03:37:42 -0300

    feat: configura socialite

---
+++ app/Http/Controllers/SocialiteController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use App\Enums\ProviderEnum;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+// use Illuminate\Http\Request;
+
+class SocialiteController extends Controller
+{
+    // #
+    public function redirect(ProviderEnum $provider)
+    {
+        return Socialite::driver($provider->value)->redirect();
+    }
+
+    // #
+    public function callback(ProviderEnum $provider)
+    {
+        $providerUser = Socialite::driver($provider->value)->user();
+
+        $user = User::where('email', $providerUser->getEmail())->firstOrFail();
+
+        $user->update([
+            'avatar_url' => $providerUser->getAvatar(),
+            'provider_id' => $providerUser->getId(),
+            'provider_name' => $provider->value,
+        ]);
+
+        Auth::login($user);
+
+        return to_route('dashboard');
+    }
+}
