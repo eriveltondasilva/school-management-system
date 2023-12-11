6effd036c27d35e33e5656e3c3f1d53dd4661aa4
Erivelton Silva <eriveltondasilva13@gmail.com>
2023-12-11 03:25:19 -0300

    feat: adiciona rotas

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\{DashboardController, ProfileController, SubjectController};
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -32,6 +32,32 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+// ##########################
+Route::middleware('auth')
+->group(function () {
+    // TODO: remover esta rota e configurar o HOME no arquivo auth.php
+    Route::redirect('/dashboard', '/painel');
+
+    // #DASHBOARD
+    Route::get('/painel', DashboardController::class)->name('dashboard');
+
+    // #SUBJECTS
+    Route::get('/disciplinas', [SubjectController::class, 'index'])->name('subject.index');
+    Route::get('/disciplinas/{id}', [SubjectController::class, 'show'])->name('subject.show');
+
+    // #BIMESTRES
+    // Route::get('/bimestres')->name('subjects');
+
+    // #CALENDAR
+    Route::get('/calendario', function () {
+        return Inertia::render('Calender');
+    })->name('calendar');
+
+    // #CALCULATOR
+    Route::get('/calculadora', function () {
+        return Inertia::render('Calculator');
+    })->name('calculator');
+});
 
 // ##########################
 Route::middleware('auth')
