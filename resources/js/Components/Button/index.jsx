1658f7d16a557be953eaaee3511490dcc8fcc9a3
Erivelton Silva <eriveltondasilva13@gmail.com>
2023-12-15 22:28:55 -0300

    feat: adiciona buttons

---
+++ resources/js/Components/Button/index.jsx
@@ -0,0 +1,51 @@
+import { twJoin } from 'tailwind-merge'
+
+export default function Button({
+  variant,
+  size,
+  full,
+  disabled,
+  children,
+  ...props
+}) {
+  //
+  const BUTTON_VARIANTS = {
+    primary: [
+      'bg-gray-800 text-white hover:bg-gray-700 focus:bg-gray-700 focus:ring-indigo-500 active:bg-gray-900',
+      'dark:bg-gray-200 dark:text-gray-800 dark:hover:bg-white dark:focus:bg-white dark:focus:ring-offset-gray-800 dark:active:bg-gray-300',
+    ],
+    secondary: [
+      'bg-white text-gray-700 hover:bg-gray-50 focus:ring-indigo-500 border-gray-300 shadow-sm',
+      'dark:border-gray-500 dark:bg-gray-800 dark:text-gray-300 dark:hover:bg-gray-700 dark:focus:ring-offset-gray-800',
+    ],
+    danger: [
+      'bg-red-600 text-white hover:bg-red-500 focus:ring-red-500 active:bg-red-700',
+      'dark:focus:ring-offset-gray-800',
+    ],
+  }
+
+  const SIZE_VARIANTS = {
+    sm: 'px-2 py-1.5 text-xs',
+    base: 'px-4 py-2 text-sm',
+    lg: 'px-5 py-2 text-base',
+    xl: 'px-6 py-2 text-lg',
+  }
+
+  return (
+    <button
+      disabled={disabled}
+      className={twJoin(
+        'font-semibold uppercase tracking-widest',
+        'inline-flex items-center',
+        'rounded-md border border-transparent',
+        'transition duration-150 ease-in-out focus:outline-none focus:ring-2 focus:ring-offset-2',
+        SIZE_VARIANTS[size ?? 'base'],
+        BUTTON_VARIANTS[variant ?? 'primary'],
+        full && 'w-full',
+        disabled && 'cursor-not-allowed opacity-25'
+      )}
+      {...props}>
+      {children}
+    </button>
+  )
+}
--- resources/js/Components/PrimaryButton.jsx
+++ resources/js/Components/PrimaryButton.jsx
@@ -1,15 +1,27 @@
-export default function PrimaryButton({ className = '', disabled, children, ...props }) {
-    return (
-        <button
-            {...props}
-            className={
-                `inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150 ${
-                    disabled && 'opacity-25'
-                } ` + className
-            }
-            disabled={disabled}
-        >
-            {children}
-        </button>
-    );
+import { twMerge } from 'tailwind-merge'
+
+export default function PrimaryButton({
+  className = '',
+  disabled,
+  children,
+  ...props
+}) {
+  return (
+    <button
+      disabled={disabled}
+      className={twMerge(
+        'text-xs font-semibold uppercase tracking-widest',
+        'inline-flex items-center',
+        'px-4 py-2',
+        'rounded-md border border-transparent',
+        'transition duration-150 ease-in-out focus:outline-none focus:ring-2 focus:ring-offset-2',
+        'bg-gray-800 text-white hover:bg-gray-700 focus:bg-gray-700 focus:ring-indigo-500 active:bg-gray-900',
+        'dark:bg-gray-200 dark:text-gray-800 dark:hover:bg-white dark:focus:bg-white dark:focus:ring-offset-gray-800 dark:active:bg-gray-300',
+        disabled && 'cursor-not-allowed opacity-25',
+        className
+      )}
+      {...props}>
+      {children}
+    </button>
+  )
 }
