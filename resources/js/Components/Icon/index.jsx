53ec50ce7c570be0087babfd694ca9eeb45c942e
Erivelton Silva <eriveltondasilva13@gmail.com>
2023-12-17 21:41:07 -0300

    feat: adiciona icons

---
+++ resources/js/Components/Icon/index.jsx
@@ -0,0 +1,103 @@
+export function ChevronDown({ className }) {
+  // className='lucide lucide-chevron-down'
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      fill='none'
+      stroke='currentColor'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      className={className}>
+      <path d='m6 9 6 6 6-6' />
+    </svg>
+  )
+}
+
+export function Menu({ className }) {
+  // className='lucide lucide-menu'
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      fill='none'
+      stroke='currentColor'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      className={className}>
+      <line x1='4' x2='20' y1='12' y2='12' />
+      <line x1='4' x2='20' y1='6' y2='6' />
+      <line x1='4' x2='20' y1='18' y2='18' />
+    </svg>
+  )
+}
+
+export function Close({ className }) {
+  // className='lucide lucide-x'
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      fill='none'
+      stroke='currentColor'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      className={className}>
+      <path d='M18 6 6 18' />
+      <path d='m6 6 12 12' />
+    </svg>
+  )
+}
+
+export function Save({ className }) {
+  // className='lucide lucide-save'
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      fill='none'
+      stroke='currentColor'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      className={className}>
+      <path d='M19 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11l5 5v11a2 2 0 0 1-2 2z' />
+      <polyline points='17 21 17 13 7 13 7 21' />
+      <polyline points='7 3 7 8 15 8' />
+    </svg>
+  )
+}
+
+export function Delete({ className }) {
+  // className='lucide lucide-trash-2'
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      fill='none'
+      stroke='currentColor'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      className={className}>
+      <path d='M3 6h18' />
+      <path d='M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6' />
+      <path d='M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2' />
+      <line x1='10' x2='10' y1='11' y2='17' />
+      <line x1='14' x2='14' y1='11' y2='17' />
+    </svg>
+  )
+}
